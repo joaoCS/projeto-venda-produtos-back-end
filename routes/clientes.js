6beffd780804df1a717fbba94635d3b18117a04c
joaoCS <joaoantoniogba@hotmail.com>
2023-03-22 17:26:24 -0300

    Adicionada as rotas de apagar e editar clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -35,4 +35,48 @@ router.get('/', async (req, res)=>{
     }
 });
 
+router.put('/edit', verifyToken, async (req, res) => {
+    const { _id, nome, endereco, cpf, telefone } = req.body;
+
+    let cliente = await ClienteModel.findOne({_id});
+
+    if(!cliente){
+        res.status(500);
+        return res.json({message: "Cliente não existe!"});
+    }
+
+    try {
+        cliente.nome = nome;
+        cliente.endereco = endereco;
+        cliente.cpf = cpf;
+        cliente.telefone = telefone;
+
+        await cliente.save();
+
+        res.json({message: "Cliente alterado com sucesso!"});
+    } 
+    catch (err) {
+        
+    }
+});
+
+router.delete('/delete', verifyToken, async (req, res) => {
+    const { _id } = req.body.cliente;
+
+    if (!_id) {
+
+        res.status(500);
+        return res.json({message: "Id de cliente não fornecido!"});
+    }
+
+    try {
+        await ClienteModel.deleteOne({_id});
+        res.json({message: "Cliente removido com sucesso!"});
+    } 
+    catch (err) {
+        res.status(500);
+        return res.json({message: "Erro ao remover cliente!", err});
+    }
+});
+
 export { router as clienteRouter }
