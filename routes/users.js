3d0091093594263662e7aacf090035d2c67658d0
joaoCS <joaoantoniogba@hotmail.com>
2023-03-24 17:10:24 -0300

    Adicionado buscar dados de e atualizar usuário

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import jwt, { verify } from 'jsonwebtoken';
 
 
 const router = express.Router();
@@ -68,7 +68,7 @@ router.post('/login', async (req, res) => {
 
 router.get('/username', verifyToken, async (req, res) => {
     try {
-        const user = await UserModel.findById(req.body.userId);
+        const user = await UserModel.findById(req.headers.userid);
         
         res.json({ username: user.username });
     } 
@@ -78,6 +78,48 @@ router.get('/username', verifyToken, async (req, res) => {
     }
 });
 
+router.get('/user/:id', verifyToken, async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        res.status(500);
+        return res.json({message: "Id do usuário não fornecido!"});
+    }
+
+    try {
+        const user = await UserModel.findById(id);
+        user.password = "";
+
+        res.json(user);
+    } 
+    catch (err) {
+        res.status(500);
+        res.json({ message: "Erro ao buscar usuário!", err });    
+    }
+});
+
+router.put('/edit', verifyToken, async (req, res) => {
+    const { _id, username, email } = req.body;
+
+    const user = await UserModel.findById(_id);
+
+    if (!user) {
+        res.status(500);
+        return res.json({message: "Usuário não encontrado!"});
+    }
+
+    try {
+        user.username = username;
+        user.email = email;
+
+        await user.save();
+
+        res.json({message: "Dados atualizados com sucesso!"});
+    } 
+    catch (err) {
+        res.status(500);
+        return res.json({message: "Erro ao atualizar dados de usuário!"});
+    }
+});
 
 export {router as userRouter};
 
